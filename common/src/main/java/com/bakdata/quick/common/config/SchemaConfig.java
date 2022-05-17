269e33a7d7d3ee9ff539d61826487c9556a30e5f
Torben Meyer <torben.meyer@bakdata.com>
2022-05-17 08:10:41 +0200

    Add copyright

--- common/src/main/java/com/bakdata/quick/common/config/SchemaConfig.java
+++ common/src/main/java/com/bakdata/quick/common/config/SchemaConfig.java
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2022 bakdata GmbH
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.bakdata.quick.common.config;
 
 import com.bakdata.quick.common.schema.SchemaFormat;
@@ -17,8 +33,8 @@ public class SchemaConfig {
      * Format of the schema to use in Quick.
      *
      * <p>
-     * This is a global configuration that is set when deploying Quick. Quick will convert GraphQL schemas
-     * to this format when user create a new topic, and register them with the Schema Registry.
+     * This is a global configuration that is set when deploying Quick. Quick will convert GraphQL schemas to this
+     * format when user create a new topic, and register them with the Schema Registry.
      * </p>
      */
     private final SchemaFormat format;
