00192567587e25742083e102bc321d47083347ff
dawidniezgodka <dawid.niezgodka@gmail.com>
2022-07-08 11:26:52 +0200

    test adjustment

--- common/src/test/java/com/bakdata/quick/common/api/client/routing/PartitionRouterTest.java
+++ common/src/test/java/com/bakdata/quick/common/api/client/routing/PartitionRouterTest.java
@@ -2,14 +2,12 @@ package com.bakdata.quick.common.api.client.routing;
 
 import com.bakdata.quick.common.testutils.TestUtils;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import okhttp3.mockwebserver.MockWebServer;
-import okhttp3.mockwebserver.RecordedRequest;
 import org.apache.kafka.common.serialization.Serdes;
 import org.junit.jupiter.api.Test;
+
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Test for Partition Router.
@@ -17,18 +15,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @MicronautTest
 public class PartitionRouterTest {
 
-    private final MockWebServer server = new MockWebServer();
-
     @Test
-    void shouldBeInitializedWithTwoDifferentHosts() throws InterruptedException {
+    void shouldBeInitializedWithTwoDifferentHosts() {
 
         final Map<Integer, String> elements = Map.of(1, "1", 2, "2");
         final Router<String> partitionRouter = new PartitionRouter<>(Serdes.String(), "dummy",
                 TestUtils.getMockPartitionFinder(), elements);
         assertThat(partitionRouter.getAllHosts()).hasSize(2);
-
-        final RecordedRequest request = server.takeRequest();
-        assertEquals("/streams/partitions", request.getPath());
     }
 
     @Test
