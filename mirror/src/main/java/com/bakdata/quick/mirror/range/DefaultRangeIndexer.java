f794326f9294595c149695f4f9e68a9ecaaa8542
Ramin Gharib <ramin.gharib@bakdata.com>
2022-10-10 09:09:34 +0200

    Update files

--- mirror/src/main/java/com/bakdata/quick/mirror/range/DefaultRangeIndexer.java
+++ mirror/src/main/java/com/bakdata/quick/mirror/range/DefaultRangeIndexer.java
@@ -72,7 +72,7 @@ public final class DefaultRangeIndexer<K, V, F> implements RangeIndexer<K, V> {
             case (ProtobufSchema.TYPE):
                 return getDefaultRangeIndexerForProtobuf(parsedSchema, rangeField, keyZeroPadder);
             default:
-                throw new MirrorTopologyException("Key value should be either integer or long");
+                throw new MirrorTopologyException("Value should be of type schema.");
         }
     }
 
